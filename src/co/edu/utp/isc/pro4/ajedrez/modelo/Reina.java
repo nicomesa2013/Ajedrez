adc2570d174cad77b91e7a3cdfcafd28ba5c9926
utp <utp@192.168.8.143>
2019-05-21 18:53:09 -0500

    Conexion movimientos con frame. Correccion movimientos. Falta que no salte trayectoria al comer

--- src/co/edu/utp/isc/pro4/ajedrez/modelo/Reina.java
+++ src/co/edu/utp/isc/pro4/ajedrez/modelo/Reina.java
@@ -58,14 +58,19 @@ public class Reina extends Ficha {
                 else if(casillaF.getFila() > casillaI.getFila()){
                     fI = fI + 1;
                 }
-                casillaC = tablero.getCasilla(cI,fI);
-                ocupada = casillaC.isOcupada();
+                casillaC = tablero.getCasilla(fI,cI);
+                if(casillaC.getColumna() != casillaF.getColumna() && casillaC.getFila() != casillaF.getFila()){
+                    ocupada = casillaC.isOcupada();
+                }
+                if(casillaC.getColumna() != casillaF.getColumna() && casillaC.getFila() != casillaF.getFila()){
+                    ocupada = casillaC.isOcupada();
+                }
                 while((casillaC.getFila() != casillaF.getFila()) && (casillaC.getColumna() != casillaF.getColumna()) && !ocupada){
-                    casillaC = tablero.getCasilla(cI,fI);
+                    casillaC = tablero.getCasilla(fI,cI);
                     ocupada = casillaC.isOcupada();
                     if (casillaF.getColumna() > casillaI.getColumna() && casillaF.getFila() > casillaI.getFila()){
-                    cI = cI + 1;
-                    fI = fI + 1;
+                        cI = cI + 1;
+                        fI = fI + 1;
                     }
                     //condiciones diagonales
                     else if(casillaF.getColumna() < casillaI.getColumna() && casillaF.getFila() > casillaI.getFila()){
@@ -94,24 +99,28 @@ public class Reina extends Ficha {
                         fI = fI + 1;
                     }
                 }
-                if(!ocupada){
-                if(casillaI.getFicha().getColor() != casillaF.getFicha().getColor()){
-                this.comer(casillaI, casillaF);
+                if(!casillaF.isOcupada()){//Que en la casilla final no haya nada    TIPO 1 (MOVIMIENTO NORMAL)
+                    if(!ocupada){//Si no hay nada en la trayectoria
+                        casillaI.setFichaNull();
+                        super.asociarFichaTablero(this, casillaF);
+                    }
+                    else{
+                        System.out.println("Hay una ficha en la trayectoria");
+                    }
                 }
-                else if(casillaI.getFicha().getColor() == casillaF.getFicha().getColor()){//Si la ficha inicial es del mismo color que la final no es valido
-                    System.out.println("Movimiento no valido porque ambas fichas son del mismo color.");
-                }
-                else if(!casillaF.isOcupada()){//Movimiento normal
-                    casillaI.setFichaNull();
-                    super.asociarFichaTablero(this, casillaF);
-                }
-                }
-                else{//Movimiento no valido por elemento en la trayectoria
-                    System.out.println("Movimiento no valido por ficha en trayectoria");
+                else if(casillaF.isOcupada()){//Que en la casilla final haya una ficha                       TIPO 2 (COMER)
+                    if(this.getColor() != casillaF.getFicha().getColor()){//Si la fichaI y la fichaF son de diferente color
+                        if(!ocupada){
+                            this.comer(casillaI,casillaF);
+                        }    
+                    }
+                    else if(this.getColor() == casillaF.getFicha().getColor()){
+                        System.out.println("Ambas casillas son del mismo color.");
+                    }
                 }
                 }
             else{
-                System.out.println("De esa forma no se mueve el alfil");
+                System.out.println("De esa forma no se mueve la reina");
             }
         }
 
